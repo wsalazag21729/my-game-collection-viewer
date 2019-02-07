1cd48aac93bb41aeb87826fd95d122160a654e92
Alejandro Saldarriaga <a.saldarriaga@globant.com>
2019-02-07 12:13:06 -0500

    Updates

---
+++ precache-manifest.b7673f3d14ac190b37cf5f75727309b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/my-game-collection-viewer/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "35d853931c851106b5f6",
+    "url": "/my-game-collection-viewer/static/js/runtime~main.35d85393.js"
+  },
+  {
+    "revision": "2ca309ed658645ee2157",
+    "url": "/my-game-collection-viewer/static/js/main.2ca309ed.chunk.js"
+  },
+  {
+    "revision": "13eeb2032981bf3107f9",
+    "url": "/my-game-collection-viewer/static/js/1.13eeb203.chunk.js"
+  },
+  {
+    "revision": "2ca309ed658645ee2157",
+    "url": "/my-game-collection-viewer/static/css/main.a073f075.chunk.css"
+  },
+  {
+    "revision": "b7601396f61ee2c47017a0522f2e6d28",
+    "url": "/my-game-collection-viewer/index.html"
+  }
+];
